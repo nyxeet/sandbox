3f25aa648a476b9971086894cebb044f7a0ef11d
Dmytro Kyrychenko <nyxeet4@gmail.com>
2023-03-16 15:01:20 +0200

    added app controller to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { CatsController } from './cats/cats.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 
 @Module({
-  imports: [CatsModule]
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
